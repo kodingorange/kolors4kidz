9138bffa09ed0da33ae76ff162c20a45fe53cfa0
Carter <turbocjwebb@gmail.com>
2023-06-12 20:44:13 -0700

    Fetched trivia question

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Trivia API
-fetch("https://the-trivia-api.com/v2/questions/", {
-    cache: "reload",
-})
+var giphy_url = "http://api.giphy.com/v1/gifs/search?q=kids&api_key=cwa0QFQaDVpqX74kWi8oygXzdghLiMJV&limit=5";
+var trivia_api = "https://the-trivia-api.com/api/question/63951fffe55fa75b149597b7";
+//var trivia_api = "https://the-trivia-api.com/api/questions?&tags=colours&difficulty=easy";
+
+// Giphy API
+fetch(giphy_url, {})
     .then(function (response) {
         return response.json();
     })
@@ -9,13 +11,11 @@ fetch("https://the-trivia-api.com/v2/questions/", {
         console.log(data);
     });
 
-// Giphy API
-fetch("http://api.giphy.com/v1/gifs/search?q=kids&api_key=cwa0QFQaDVpqX74kWi8oygXzdghLiMJV&limit=5", {
-    cache: "reload",
-    })
+// Trivia API
+fetch(trivia_api, {})
     .then(function (response) {
-            return response.json();
+        return response.json();
     })
     .then(function (data) {
-            console.log(data);
+        console.log(data);
     });
