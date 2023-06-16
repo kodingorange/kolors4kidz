98eeec87c993a5fc12e99fc7c1b029e1fe0f1f92
kodingorange <kodnigorange@.com>
2023-06-15 19:16:34 -0700

    l

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var giphy_url = "http://api.giphy.com/v1/gifs/search?q=kids&api_key=cwa0QFQaDVpq
 var trivia_api = "https://the-trivia-api.com/api/question/63951fffe55fa75b149597b7";
 //var trivia_api = "https://the-trivia-api.com/api/questions?&tags=colours&difficulty=easy";
 
+
 // Giphy API
 fetch(giphy_url, {})
     .then(function (response) {
