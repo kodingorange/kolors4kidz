1e0f40216b397eae391899eb4e96fa9f5713fccc
kodingorange <kodnigorange@.com>
2023-06-19 19:52:02 -0700

    hello

--- script.js
+++ script.js
@@ -87,3 +87,4 @@ var question10 = {
 //Randomizing Quiz (w/ help from Michael)
 var quiz = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
 var randomQuiz = [];
+
